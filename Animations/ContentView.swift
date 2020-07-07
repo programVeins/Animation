b5362c3d7c39a4452577df7d8b6f4ff6c49bbbca
Sabesh Bharathi <sabesh5601@gmail.com>
2020-07-07 11:05:11 +0530

    Added Origin

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,13 +9,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var dragAmount = CGSize.zero
+
     var body: some View {
-        Text("Hello, World!")
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .shadow(radius: 10)
+            .offset(dragAmount)
+            .gesture(DragGesture()
+                .onChanged { value in
+                    self.dragAmount = value.translation
+            }
+            .onEnded { _ in
+                withAnimation(.interpolatingSpring(stiffness: 170, damping: 10)) {
+                    self.dragAmount = .zero
+                }
+            })
+    }
+}
+
+struct ContentView2: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num)/50))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { self.dragAmount = $0.translation }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+                }
+        )
+    }
+}
+
+struct ContentView3: View {
+    
+    @State private var isShowingRed = false
+
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView2()
+            ContentView3()
+        }
+        
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# Animation
+
+### 100DaysOfSwiftUI - Project 6
+
+Second Technique project that involves various depths of animations - implicit, binding and explicit. I am particularly very fond of a card animation, that inspired me to go beyond and make a partial wallet app. UI/UX Ftw 🧼
